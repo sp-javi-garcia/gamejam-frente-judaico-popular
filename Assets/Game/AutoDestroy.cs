cc2d20d56f51e37df2c1952f3ae670a611f2a7b0
Javier Garcia <javi.garcia@socialpoint.es>
2014-10-25 23:58:25 +0200

    Save prefabs + autodestroy

---
+++ Assets/Game/AutoDestroy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoDestroy : MonoBehaviour
+{
+    public float DestroyTime = 5f;
+
+    void Update ()
+    {
+        DestroyTime -= Time.deltaTime;
+        if (DestroyTime <= 0f)
+            Destroy(gameObject);
+    }
+}
