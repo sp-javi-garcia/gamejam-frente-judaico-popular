394f806019c64769c3318e44b38c356ec48ac633
Mario Quesada <mario.quesada@socialpoint.es>
2014-10-25 03:16:00 +0200

    added audio

---
+++ Assets/ZombieAudioManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieAudioManager : MonoBehaviour 
+{
+	[SerializeField]
+	AudioClip _biteClip;
+
+	public void PlayBite()
+	{
+		AudioSource.PlayClipAtPoint(_biteClip, transform.position);
+	}
+}
