f87e8b127bdc09262535dc6df8cc995a0b66e4e9
sp-juan-sabater <juan.sabater@socialpoint.es>
2014-10-25 16:52:50 +0200

    no message

---
+++ Assets/External/Scripts/Animated_Texture.js
@@ -0,0 +1,7 @@
+﻿var scrollSpeed :  float = .5;
+var offset : float;
+
+function Update (){
+	offset += (Time.deltaTime*scrollSpeed)/10.0;
+	renderer.material.SetTextureOffset ("_MainTex", Vector2(-offset, 0));
+	}
